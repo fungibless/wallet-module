428c659723c8306636406c9716f59fddd224a13a
Erani Seo <seoerani@gmail.com>
2022-06-19 20:39:15 +0900

    Add Shorten Address Util

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,9 @@
 import { DefaultWalletModule as WalletModule, WalletKind } from './wallet-module';
 import { ethers } from 'ethers';
 export { WalletModule, WalletKind, ethers };
+
+export function shortenAddress(address: string, lengthToLeave: number) {
+    if (address) {
+        return `${address.substring(0, lengthToLeave + 1)}...${address.substring(address.length - lengthToLeave, address.length)}`;
+    }
+}
